931a48b699faa496b03e696c677133c0942c115d
unknown <pmzara@hotmail.com>
2015-09-20 12:53:21 +1000

    add dropdown

--- src/app/app.js
+++ src/app/app.js
@@ -12,82 +12,110 @@ angular.module('open-salary-app', ['ui.router', 'highcharts-ng', 'verify', 'add'
 	})
 	.controller('AppCtrl', function ($scope, $http) {
 
-		var chartConfig = {
+		$scope.chartOption = 1;
 
-			options: {
-				//This is the Main Highcharts chart config. Any Highchart options are valid here.
-				//will be overriden by values specified below.
-				chart: {
-					type: 'column'
-				},
-				tooltip: {
-					style: {
-						padding: 10,
-						fontWeight: 'bold'
+		var chartConfig = {};
+
+		$scope.generateChart = function(option) {
+			chartConfig = {
+
+				options: {
+					//This is the Main Highcharts chart config. Any Highchart options are valid here.
+					//will be overriden by values specified below.
+					chart: {
+						type: 'column'
+					},
+					tooltip: {
+						style: {
+							padding: 10,
+							fontWeight: 'bold'
+						}
 					}
-				}
-			},
-			//The below properties are watched separately for changes.
-
-			//Series object (optional) - a list of series using normal highcharts series options.
-			/*series: [
-				{
-					name: 'Female',
-					data: [60000]
 				},
-				{
-					name: 'Male',
-					data: [80000]
-				}
-			],*/
-			title: {
-				text: 'Average Salary per Gender'
-			},
-			loading: true,
-			//Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-			//properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
-			xAxis: {
-				labels: {
-					enabled: false
-				}
-			},
-			yAxis: {
-				min: 60000,
+				//The below properties are watched separately for changes.
+
+				//Series object (optional) - a list of series using normal highcharts series options.
+				/*series: [
+					{
+						name: 'Female',
+						data: [60000]
+					},
+					{
+						name: 'Male',
+						data: [80000]
+					}
+				],*/
 				title: {
-					text: 'Salary'
+					text: getChartTitle($scope.chartOption)
+				},
+				loading: true,
+				//Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+				//properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
+				xAxis: {
+					labels: {
+						enabled: false
+					}
+				},
+				yAxis: {
+					min: 60000,
+					title: {
+						text: 'Salary'
+					}
+				},
+
+				tooltip: {
+					formatter: function() {
+						return '<b>'+ this.x +'</b><br/>'+
+							this.series.name +': '+ this.y +'<br/>'+
+							'Diff: '+ Math.abs(this.point.stackTotal-this.y);
+					}
+				},
+				//Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
+				//function (optional)
+				func: function (chart) {
+					//setup some logic for the chart
 				}
-			},
+			};
 
-			tooltip: {
-				formatter: function() {
-					return '<b>'+ this.x +'</b><br/>'+
-						this.series.name +': '+ this.y +'<br/>'+
-						'Diff: '+ Math.abs(this.point.stackTotal-this.y);
-				}
-			},
-			//Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
-			//function (optional)
-			func: function (chart) {
-				//setup some logic for the chart
-			}
-		};
+			$http.get('http://localhost:8000').
+				then(function (response) {
+					var data = response.data;
+					// TODO: calculate data here
+					$scope.chartConfig = angular.extend(chartConfig, {
+						loading: false,
+						//series: getGenderAverage(data)
+						series: getChartData(data, $scope.chartOption)
+					});
+				}, function (error) {
 
-		$scope.chartConfig = chartConfig;
-
-		$http.get('http://localhost:8000').
-			then(function (response) {
-				var data = response.data;
-				// TODO: calculate data here
-				$scope.chartConfig = angular.extend(chartConfig, {
-					loading: false,
-					//series: getGenderAverage(data)
-					series: getEthnicAverage(data)
 				});
-			}, function (error) {
+		}
 
-			});
+		$scope.chartChange = function() {
+			$scope.generateChart($scope.chartOption);
+	 	};
+
+		$scope.generateChart($scope.chartOption);
 
 	});
+
+function getChartTitle(option) {
+	if (option == 1) {
+		return 'Average Salary per Gender';
+	}
+	if (option == 2) {
+		return 'Average Salary per Ethnicity';
+	}
+}
+
+function getChartData(data, option) {
+	if(option == 1) {
+		return getGenderAverage(data);
+	}
+	if(option == 2) {
+		return getEthnicAverage(data);
+	}
+}
 
 function getGenderAverage(data) {
 	var femaleTotal = 0, maleTotal = 0, maleCount = 0, femaleCount = 0;
@@ -142,8 +170,6 @@ function getEthnicAverage(data) {
 	for(var key in ethnicities) {
 		series.push({name: key, data: [Math.floor(ethnicities[key].total/ethnicities[key].count)]})
 	}
-
-	console.info(JSON.stringify(series));
 
 	return series;
 }
